b301428d582661fd285d895ea704b402caa31c0b
Teako Warfield-Graham <teakowg@gmail.com>
2019-09-28 22:18:00 -0600

    Trying out css gradient animation

--- src/index.js
+++ src/index.js
@@ -22,19 +22,12 @@ let cubeA, cubeB, group, domEvents;
 
 console.log('hello from group.js');
 
-window.addEventListener('load', (event) => {
-  container = document.getElementById('container');
-  init()
-  animate()
-});
-
-
 
 function init(){
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
   camera.position.z = 10;
-  scene.background = new THREE.Color(0xCCCCCC);
+  // scene.background = new THREE.Color(0xCCCCCC);
   // setting alpha to true enables it to pull background color
   renderer = new THREE.WebGLRenderer( {alpha: true} );
   renderer.setSize( window.innerWidth, window.innerHeight );
@@ -94,10 +87,50 @@ function testGroup(){
   cubeB = new THREE.Mesh( geometryB, materialB ); //black
   cubeB.position.set( -1, -1, 0 );
 
+
+
+  // var url		= 'http://jeromeetienne.github.io/threex/'
+  // var linkify	= THREEx.Linkify(domEvents, cubeB, url, true)
+
+
+  //create a group and add the two cubes
+  //These cubes can now be rotated / scaled etc as a group
+  group = new THREE.Group();
+  group.add( cubeA );
+  group.add( cubeB );
+
+  scene.add( group );
+}
+
+
+
+
+// DEFAULT SCENE EVENT LISTENERS
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+function animate(){
+  // add object animation here
+  group.rotation.y += 0.005;
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+window.addEventListener('load', (event) => {
+  container = document.getElementById('container');
+  init()
+  animate()
+
+  // DOMEVENTS
   domEvents	= new THREEx.DomEvents(camera, renderer.domElement)
 
   domEvents.addEventListener(cubeA, 'click', function(event){
-  	console.log('you clicked on green cubeA')
+    console.log('you clicked on green cubeA')
     cubeA.material.color.setHex( 0x00ff00 )
     // window.open("https://www.w3schools.com");
   }, false)
@@ -117,39 +150,4 @@ function testGroup(){
   domEvents.addEventListener(cubeB, 'click', function(event){
     console.log('you clicked on black cubeB')
   }, false)
-
-  // var url		= 'http://jeromeetienne.github.io/threex/'
-  // var linkify	= THREEx.Linkify(domEvents, cubeB, url, true)
-
-
-  //create a group and add the two cubes
-  //These cubes can now be rotated / scaled etc as a group
-  group = new THREE.Group();
-  group.add( cubeA );
-  group.add( cubeB );
-
-  scene.add( group );
-}
-
-
-// DOMEVENTS
-// domEvents.addEventListener(cubeA, 'click', function(event){
-// 	console.log('you clicked on the mesh')
-// }, false)
-
-// DEFAULT SCENE EVENT LISTENERS
-
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-
-function animate(){
-  // add object animation here
-  group.rotation.y += 0.005;
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-}
+});
